c2fe039f0d2aa154a8eaf1e599df0112f4e5684f
AnkitKSinha <ankit.sinha2000@gmail.com>
2017-02-17 21:45:06 +0530

    Moved coming-soon site to coming-soon + Main website is now root

--- js/main.js
+++ js/main.js
@@ -1,169 +1,130 @@
-"use strict";
-/* 0. Initialization */
-// Get height on Window resized
-$(window).on('resize',function(){
-    var slideHeight = $('.slick-track').innerHeight();
-	return false;
-});
 
+function main() {
 
-// Smooth scroll <a> links 
-var $root = $('html, body');
-$('a.s-scroll').on('click',function() {
-    var href = $.attr(this, 'href');
-    $root.animate({
-        scrollTop: $(href).offset().top
-    }, 500, function () {
-        window.location.hash = href;
-    });
-    return false;
-});
+(function () {
+   'use strict';
 
-// Page Loader : hide loader when all are loaded
-$(window).load(function(){
-    $('#page-loader').addClass('hidden');
-});
+   /* ==============================================
+  	Testimonial Slider
+  	=============================================== */ 
 
-
-/* 1. Clock attribute */
-
-var dateReadableText = 'Upcoming date';
-    if($('.site-config').attr('data-date-readable') && ($('.site-config').attr('data-date-readable') != '')){
-        $('.timeout-day').text('');
-        dateReadableText = $('.site-config').attr('data-date-readable');        
-        $('.timeout-day').text(dateReadableText);
-    }
-$('.clock-countdown').downCount({
-    date: $('.site-config').attr('data-date'),
-    offset: +10
-}, function () {
-    //callback here if finished
-    //alert('YES, done!');
-    var zerodayText = 'An upcoming date';
-    if($('.site-config').attr('data-zeroday-text') && ($('.site-config').attr('data-zeroday-text') != '')){
-        $('.timeout-day').text('');
-        zerodayText = $('.site-config').attr('data-zeroday-text'); 
-    }
-    $('.timeout-day').text(zerodayText);
-});
-
-
-/* 2. Background for page / section */
-
-var background = '#ccc';
-var backgroundMask = 'rgba(255,255,255,0.92)';
-var backgroundVideoUrl = 'none';
-
-/* Background image as data attribut */
-var list = $('.bg-img');
-
-for (var i = 0; i < list.length; i++) {
-	var src = list[i].getAttribute('data-image-src');
-	list[i].style.backgroundImage = "url('" + src + "')";
-	list[i].style.backgroundRepeat = "no-repeat";
-	list[i].style.backgroundPosition = "center";
-	list[i].style.backgroundSize = "cover";
-}
-
-/* Background color as data attribut */
-var list = $('.bg-color');
-for (var i = 0; i < list.length; i++) {
-	var src = list[i].getAttribute('data-bgcolor');
-	list[i].style.backgroundColor = src;
-}
-
-/* Background slide show Background variables  */
-var imageList = $('.slide-show .img');
-var imageSlides = [];
-for (var i = 0; i < imageList.length; i++) {
-	var src = imageList[i].getAttribute('data-src');
-	imageSlides.push({src: src});
-}
-
-
-/* Slide Background variables */
-var isSlide = false;
-var slideElem = $('.slide');
-var arrowElem = $('.p-footer .arrow-d');
-var pageElem = $('.page');
-
-/* 3. Init all plugin on load */
-$(document).ready(function() {
-
-    $("#submit-message").click(function(){
-        $("#write-to-us").submit();
-    });
-	/* Init console to avoid error */
-	var method;
-    var noop = function () {};
-    var methods = [
-        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-        'timeStamp', 'trace', 'warn'
-    ];
-    var length = methods.length;
-    var console = (window.console = window.console || {});
-
-    while (length--) {
-        method = methods[length];
-
-        // Only stub undefined methods.
-        if (!console[method]) {
-            console[method] = noop;
+  	$('a.page-scroll').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top - 40
+            }, 900);
+            return false;
+          }
         }
-    }
-	
-	/* Init Slidesow background */
-	 $('.slide-show').vegas({
-        delay: 5000,
-        shuffle: true,
-        slides: imageSlides,
-    	//transition: [ 'zoomOut', 'burn' ],
-		animation: [ 'kenburnsUp', 'kenburnsDown', 'kenburnsLeft', 'kenburnsRight' ]
-    });
-	
-	/* Init video background */
-	//$('.video-container video, .video-container object').maximage('maxcover');
-	
-	/* Init youtube video background */
-	//if(backgroundVideoUrl != 'none'){
-        
-        //disable video background for smallscreen
-        /*
-        if($(window).width() > 640){
-          $.okvideo({ source: backgroundVideoUrl,
-                    adproof: true
-                    });
+      });
+
+    /*====================================
+    Show Menu on Book
+    ======================================*/
+    $(window).bind('scroll', function() {
+        var navHeight = $(window).height() - 100;
+        if ($(window).scrollTop() > navHeight) {
+            $('.navbar-default').addClass('on');
+        } else {
+            $('.navbar-default').removeClass('on');
         }
-        */
-   // }
-	
-	/** Init fullpage.js */
-    $('#mainpage').fullpage({
-		menu: '#qmenu',
-		anchors: ['home', 'theme', 'tedx', 'contact'],
-//        verticalCentered: false,
-//        resize : false,
-//		responsive: 900,
-		scrollOverflow: true,
-        css3: false,
-        navigation: true,
-		onLeave: function(index, nextIndex, direction){
-			arrowElem.addClass('gone');
-			pageElem.addClass('transition');
-//			$('.active').removeClass('transition');
-			slideElem.removeClass('transition');
-			isSlide = false;
-		},
-        afterLoad: function(anchorLink, index){
-			arrowElem.removeClass('gone');
-			pageElem.removeClass('transition');
-			if(isSlide){
-				slideElem.removeClass('transition');
-			}
-		},
-		
-        afterRender: function(){}
     });
-});
+
+    $('body').scrollspy({ 
+        target: '.navbar-default',
+        offset: 80
+    })
+
+  	$(document).ready(function() {
+       $("html").niceScroll({
+        cursorcolor:"rgb(230, 43,30)",
+        cursorwidth:"10px",
+        cursorborderradius : "1px",
+        cursorborder: "1px solid rgb(230, 43, 30)"
+      });
+
+  	  $("#team").owlCarousel({
+  	 
+  	      navigation : false, // Show next and prev buttons
+  	      slideSpeed : 300,
+  	      paginationSpeed : 400,
+  	      autoHeight : true,
+  	      itemsCustom : [
+				        [0, 1],
+				        [450, 2],
+				        [600, 2],
+				        [700, 2],
+				        [1000, 4],
+				        [1200, 4],
+				        [1400, 4],
+				        [1600, 4]
+				      ],
+  	  });
+
+  	  $("#clients").owlCarousel({
+  	 
+  	      navigation : false, // Show next and prev buttons
+  	      slideSpeed : 300,
+  	      paginationSpeed : 400,
+  	      autoHeight : true,
+  	      itemsCustom : [
+				        [0, 1],
+				        [450, 2],
+				        [600, 2],
+				        [700, 2],
+				        [1000, 4],
+				        [1200, 5],
+				        [1400, 5],
+				        [1600, 5]
+				      ],
+  	  });
+
+      $("#testimonial").owlCarousel({
+        navigation : false, // Show next and prev buttons
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true
+        });
+
+  	});
+
+  	/*====================================
+    Portfolio Isotope Filter
+    ======================================*/
+    $(window).load(function() {
+        var $container = $('#lightbox');
+        $container.isotope({
+            filter: '*',
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+        });
+        $('.cat a').click(function() {
+            $('.cat .active').removeClass('active');
+            $(this).addClass('active');
+            var selector = $(this).attr('data-filter');
+            $container.isotope({
+                filter: selector,
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false
+                }
+            });
+            return false;
+        });
+
+    });
+
+
+
+}());
+
+
+}
+main();
